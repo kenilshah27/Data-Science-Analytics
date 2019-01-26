b0de59ee22a7beed2105ab2d27afe351de2a5fc5
Kenil Shah <kenilshah0861@gmail.com>
2019-01-26 01:00:24 -0600

    Update DecisionTreeRegression.py

--- Templates/Python/Regression/DecisionTreeRegression.py
+++ Templates/Python/Regression/DecisionTreeRegression.py
@@ -68,6 +68,18 @@ r2 = r2_score(test_Y,y_pred)
 plt.scatter(model.predict(train_X),model.predict(train_X) - train_Y, c = 'b')
 plt.hlines(y = 0,xmin = min(model.predict(train_X)),xmax = max(model.predict(train_X)))
 
+#Visualize the Decision Tree
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+from sklearn.tree import export_graphviz
+import pydotplus
+dot_data = StringIO()
+export_graphviz(model, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+Image(graph.create_png())
+
 #Saving the output file
 y_pred = pd.Series(y_pred, name = 'target variable name')
 submission = pd.DataFrame(data='The unique indentity of test dataset')
